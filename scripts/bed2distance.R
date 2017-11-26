3ec23b01ded532bea9f9b9e2c7304a60866de7ac
Yanyu Liang <yanyul@gmail.uchicago.edu>
2017-11-26 09:06:05 -0600

    added

--- scripts/bed2distance.R
+++ scripts/bed2distance.R
@@ -13,7 +13,14 @@ opt_parser <- OptionParser(option_list=option_list)
 opt <- parse_args(opt_parser)
 
 library(dplyr)
-
+inter <- tryCatch({
+  read.table(opt$intersection, header = F, sep = '\t')
+}, error <- function(err){
+  gz <- gzfile(opt$output, "w")
+  write.table(data.frame(), gz, col.names = T, row.names = F, quote = F, sep = '\t')
+  close(gz)
+  quit()
+})
 inter <- read.table(opt$intersection, header = F, sep = '\t')
 inter$junction.pos <- (inter$V5 + inter$V6 - 1) / 2
 inter$distance <- abs(inter$V2 - inter$junction.pos)
